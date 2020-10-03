f0dd1036aa18128656d1b2ec49512d11e020d810
Margarita Belush <margarit2@ya.ru>
2020-10-03 22:10:31 +0300

    add cod

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+    } else if (array.length  ==0) {
+    return 0;
+    }
+  let minArr = Math.min.apply(null, array);
+  return minArr;
+  
 }
+
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined) {
+    return 0;
+    } else if (array.length  ==0) {
+    return 0;
+    }
+   return Math.max.apply(null, array);
+  
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array === undefined) {
+    return 0;
+    } else if (array.length  ==0) {
+    return 0;
+    } 
+  let sum = (array).reduce((a, b) => a + b, 0);
+  let result = sum / (array).length;
+  return result
+  };
+
+  
